0345ad3b12719cefb867e0fe04847307015637f0
theblind <ruanbowen@gmail.com>
2014-01-16 14:43:22 +0800

    redesigh model relationship

--- website/test_phase/models.py
+++ website/test_phase/models.py
@@ -14,36 +14,10 @@ class IaaSProvider(models.Model):
 		return description
 
 
-# create model UnixBench to represent
-# this benchmark's detail
-class UnixBench(models.Model):
-	# machine score for serail test
-	serialScore = models.IntegerField()cour
-
-	# machine score for parallel test
-	parallelScore = models.IntegerField()
-
-
-# create model Phoronix to represent
-# this benchmark's detail
-class Phoronix(models.Model):
-	# test result for 7-zip compression
-	# result unit is MIPS
-	compressionResult = models.IntegerField()
-
-	# test result for postgreSQL benchmark
-	# result unit is TPS
-	pgbenchResult = models.IntegerField()
-
-
 # create model machine to represent virtual machine
 class Machine(models.Model):
 	# many to one relationship with IaaS Provider
 	provider = models.ForeignKey(IaaSProvider)
-	# many to many relationship with UnixBench
-	unixBenchResults = models.ManyToManyField(UnixBench)
-	# many to many relationship with Phoronix Test Suite
-	phoronixResults = models.ManyToManyField(Phoronix)
 
 	# machine name
 	name = models.CharField(max_length = 30)
@@ -72,3 +46,37 @@ class Machine(models.Model):
 		)
 	scale = models.CharField(max_length = 1,
 	 choices = VIRTUAL_MACHINE_SCALE)
+
+
+# create model UnixBench to represent
+# this benchmark's detail
+class UnixBench(models.Model):
+	# many to one relationship with machine
+	machine = models.ForeignKey(Machine)
+
+	# machine score for serail test
+	serialScore = models.IntegerField()
+	
+	# machine score for parallel test
+	parallelScore = models.IntegerField()
+
+	# timestamps for single test
+	created_at = models.DateField(auto_now_add = True)
+
+
+# create model Phoronix to represent
+# this benchmark's detail
+class Phoronix(models.Model):
+	# many to one relationship with machine
+	machine = models.ForeignKey(Machine)
+
+	# test result for 7-zip compression
+	# result unit is MIPS
+	compressionResult = models.IntegerField()
+
+	# test result for postgreSQL benchmark
+	# result unit is TPS
+	pgbenchResult = models.IntegerField()
+
+	# timestamps for single test
+	created_at = models.DateField(auto_now_add = True)
