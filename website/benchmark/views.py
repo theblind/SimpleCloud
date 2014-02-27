ad260df5fdde267a81ebe13d3692090c8789066d
theblind <ruanbowen@gmail.com>
2014-02-27 08:51:49 +0800

    finish receive unixbench result

--- website/benchmark/views.py
+++ website/benchmark/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
+from benchmark.models import Instance, UnixBench
 
 # Create your views here.
 def parseUnixBenchResult(request, instanceID):
-	print request.POST
 	if request.method == 'POST':
 		try:
-			print "start"
+			#print request.POST
+
 			key = request.POST['key']
-			instance = Instance.get(pk = instanceID)
+			instance = Instance.objects.get(pk = instanceID)
 			if instance.key == key:
 				serialScore = request.POST['serialScore']
 				parallelScore = request.POST['parallelScore']
-				
 
 				ub = UnixBench(instance = instance, serialScore = serialScore, parallelScore = parallelScore)
 				ub.save()
-				print "well done"
 		except KeyError:
 			print 'Error: can\'t parse UnixBench result'
 
