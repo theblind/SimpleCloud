0504142399648cac7bf81f0e5b591bd2c9f76b93
theblind <ruanbowen@gmail.com>
2014-03-28 09:15:02 +0800

    in contoller layer, add phoronix result parser

--- website/benchmark/urls.py
+++ website/benchmark/urls.py
@@ -3,4 +3,5 @@ from benchmark import views
 
 urlpatterns = patterns('',
 	url(r'^instance/(?P<instanceID>\d+)/unixbench$', views.parseUnixBenchResult, name = 'unixbench'),
+	url(r'^instance/(?P<instanceID>\d+)/phoronix$', views.parsePhoronixResult, name = 'phoronix'),
 )
--- website/benchmark/views.py
+++ website/benchmark/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from benchmark.models import Instance, UnixBench
+from benchmark.models import Instance, UnixBench, Phoronix
 
-# Create your views here.
+# save vm's unixbench result into database
 def parseUnixBenchResult(request, instanceID):
 	if request.method == 'POST':
 		try:
@@ -13,6 +13,24 @@ def parseUnixBenchResult(request, instanceID):
 
 				ub = UnixBench(instance = instance, serialScore = serialScore, parallelScore = parallelScore)
 				ub.save()
+				return render(request, 'benchmark/succeed.html')
+		except KeyError:
+			print 'Error: can\'t parse UnixBench result'
+
+	return render(request, 'benchmark/failed.html')
+
+# save vm's phoronix result into database
+def parsePhoronixResult(request, instanceID):
+	if request.method == 'POST':
+		try:
+			hashKey = request.POST['hashKey']
+			instance = Instance.objects.get(pk = instanceID)
+			if instance.hashKey == hashKey:
+				compressionResult = request.POST['compressionResult']
+				pgbenchResult = request.POST['pgbenchResult']
+
+				phoronix = Phoronix(instance = instance, compressionResult = compressionResult, pgbenchResult = pgbenchResult)
+				phoronix.save()
 				return render(request, 'benchmark/succeed.html')
 		except KeyError:
 			print 'Error: can\'t parse UnixBench result'
