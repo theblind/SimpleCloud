b08e92836f7dc010913d986ee5ae7daaab85565d
theblind <ruanbowen@gmail.com>
2014-07-22 20:47:50 +0800

    merge conflict

--- website/util/test.py
+++ website/util/test.py
@@ -1,16 +1,15 @@
 from IaaS.middleware import IaaSConnection
 from IaaS import usertoken
-from pprint import pprint
 import boto.ec2 as ec2
+
+from Crypto.PublicKey import RSA
 
 if __name__ == '__main__':
 	provider = "ec2"
 	token = usertoken.get_access_key(None, provider)
 	conn = IaaSConnection(token, provider, "us-west-2")
 
-	reservation = conn.buy_instance_temporary('ami-fa9cf1ca', 't1.micro')
-	
-	serverID = reservation.instances[0].id
-	print serverID
-	address = reservation.instances[0].private_ip_address
-	print address
+	key = RSA.generate(2048)
+	content = key.publickey().exportKey('OpenSSH')
+	result = conn.conn.import_key_pair("test", content)
+	print result
