4578bae537f92c2de9bc8858c9f7bd9c627cb345
theblind <ruanbowen@gmail.com>
2014-07-22 21:02:50 +0800

    fix merge error

--- website/clients/models.py
+++ website/clients/models.py
@@ -210,203 +210,25 @@ class ClientEnvironment(models.Model):
 		connection.import_key_pair(key_name, public_key)
 
 	# create property for current environment
-	def createProperty(self, name = "", value = ""):
-		cep = ClientEnvironmentProperty(ClientEnvironment = self, name = name, value = value)
+	def createProperty(self,name,value):
+		cep, created = ClientEnvironmentProperty.objects.get_or_create(environment = self, name = name)
+		cep.value = value
 		cep.save()
 
 	def isActive(self):
-    # use email address to identify a client
-    email = models.EmailField(unique = True)
-    password = models.CharField(max_length = 32)
-    name = models.CharField(max_length = 30)
-    fullName = models.CharField(max_length = 60)
-
-    org = models.CharField(max_length = 60)
-    country = models.CharField(max_length = 60)
-    state = models.CharField(max_length = 60)
-    city = models.CharField(max_length = 60)
-    phone = models.CharField(max_length = 60)
-
-    status = models.CharField(max_length = 100)
-    isBilled = models.SmallIntegerField(default = 0)
-    isActive = models.SmallIntegerField(default = 0)
-    is_staff = models.SmallIntegerField(default = 0)
-
-    comments = models.TextField()
-    priority = models.IntegerField(default = 0)
-    loginAttempts = models.IntegerField(default = 0)
-
-    dtAdded = models.DateTimeField(auto_now_add = True)
-    dtDue = models.DateTimeField(null = True)
-    dtLastLoginAttempt = models.DateTimeField(null = True)
-    last_login = models.DateTimeField(null = True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'fullName']
-    objects = ClientUserManager()
-    def get_full_name(self):
-        return self.fullName
-    def get_short_name(self):
-        return self.name
-    def set_password(self, password):
-        self.password = hashers.make_password(password, None, 'unsalted_md5')
-    def check_password(self, password):
-        return hashers.check_password(password, self.password)
-    def has_module_perms(self, app_label):
-        return self.is_staff
-    def has_perm(self, app_label):
-        return self.is_staff
-    def is_authenticated(self):
-        return True
-    @property
-    def is_active(self):
-        return self.isActive
-
-
-    # create farm for this client
-    def createFarm(self, name = "", comments = ""):
-        import cloudmanagers.models as cloudmanagers
-        newFarm = cloudmanagers.Farm(client=self, createdByEmail=self.email, name=name, comments=comments)
-        newFarm.save()
-        return newFarm
-
-    # get all farms which belong to the client
-    def getAllFarms(self):
-        return list(self.farm_set.all())
-
-    # get all servers which belong to the client
-    def getAllServers(self):
-        servers = []
-        try:
-            farmSet = self.getAllFarms()
-            for farm in farmSet:
-                servers.extend(farm.getAllServers())
-            return servers
-        except:
-            return []
-
-    # collect access key to bind an environment
-    def bindingEnvironment(self, manufacture, **kwargs):
-        if manufacture.name == "ec2": 
-            token = {
-                "access_id": kwargs['access_id'],
-                "access_key": kwargs['access_key']
-            } 
-            connection = IaaSConnection(token, manufacture.name, "us-west-2") 
-            try:
-                regions = connection.conn.get_all_regions()
-            except Exception, e:
-                return False
-
-            newEnvironment, created = ClientEnvironment.objects.get_or_create(client = self, manufacture = manufacture)
-            if created:
-                newEnvironment.setActive()
-                newEnvironment.save()
-
-            for (name, value) in kwargs.items():
-                newEnvironment.createProperty(name = name, value = value)
-
-            return True
-
-
-    # get all environments which are active
-    def getAllEnvironments(self):
-        result = []
-        environments = self.environments.all()
-        for env in environments:
-            if env.isActive():
-                result.append(env)
-        return result
-
-    # get all environsments' properties in following format:
-    # result = [{ "manufacture": "ec2", "secret_key": "", "access_key": "" },
-    #            { "manufacture": "azure", "secret_key": "", "access_key": "" }]
-    def getAllEnvironmentsProperties(self):
-        result = []
-        environments = self.getAllEnvironments()
-
-        for env in environments:
-            result.append(env.getAllProperties())
-
-        return result
-
-    # get properties in spesicif environment
-    def getPropertiesByManufacture(self, manufacture):
-        environment = self.environments.get(manufacture = manufacture)
-        return environment.getAllProperties()
-
-    # get sshkey in specific environment
-    def getSSHKeysByManufacture(self, manufacture):
-        environment = self.environments.get(manufacture = manufacture)
-        return environment.getAllSSHKeys()
-
-    # get all sshkeys in every environment
-    def getAllEnvironmentsSSHKeys(self):
-        result = []
-        environments = self.getAllEnvironments()
-        for env in environments:
-            result.extend(env.getAllSSHKeys())
-
-        return result
-
-    def getAllEnvironmentsAvailableRoles(self):
-        result = []
-        environments = self.getAllEnvironments()
-
-        for env in environments:
-            result.extend(env.getAllAvailableRoles())
-
-        return result
-
-    def getAllMessages(self):
-        result = {}
-        result['sysmessage_list'] = []
-        result['promessage_list'] = []
-
-        messages = self.message_set.order_by('-dtAdded')
-        for message in messages:
-            if message.messageType == "S":
-                result['sysmessage_list'].append(message.getDetails())
-            elif message.messageType == "P":
-                result['promessage_list'].append(message.getDetails())
-
-        return result
-
-# cloud environment info
-class ClientEnvironment(models.Model):
-    # which manufacture
-    manufacture = models.ForeignKey(Manufacture)
-    # which client
-    client = models.ForeignKey(Client, related_name = "environments")
-
-    # set status to identify this environment's activeness
-    ACTIVE = 'A'
-    INACTIVE = 'I'
-    BINDING_STATUS = (
-        (ACTIVE, 'Active'),
-        (INACTIVE, 'Inactive'),
-    )
-    status = models.CharField(max_length = 1, choices = BINDING_STATUS, default = INACTIVE)
-
-    def createProperty(self,name,value):
-        cep, created = ClientEnvironmentProperty.objects.get_or_create(environment = self, name = name)
-        cep.value = value
-        cep.save()
-
-    def isActive(self):
 		return self.status == self.ACTIVE
 
-    def setActive(self):
+	def setActive(self):
 		self.status = self.ACTIVE
 
-    def setInactive(self):
+	def setInactive(self):
 		self.status = self.INACTIVE
 
 	# return sshkeys in following format:
 	# result = [ {"manufacture": "ec2", "privateKey": "", "publicKey": "", ...},
 	#			{"manufacture": "ec2", "privateKey": "", "publicKey": "", ...},
 	#			... ]
-    def getAllSSHKeys(self):
+	def getAllSSHKeys(self):
 		result = []
 		sshkeys = self.sshkey_set.all()
 
@@ -419,7 +241,7 @@ class ClientEnvironment(models.Model):
 
 	# return properties in following format:
 	# info = { "manufacture": "ec2", "secret_key": "", "access_key": "" }
-    def getAllProperties(self):
+	def getAllProperties(self):
 		info = {}
 		info["manufacture"] = self.manufacture.name
 
@@ -431,7 +253,7 @@ class ClientEnvironment(models.Model):
 		return info
 
 	# return available roles for current environment
-    def getAllAvailableRoles(self):
+	def getAllAvailableRoles(self):
 		import cloudmanagers.models as cloudmanagers
 
 		return cloudmanagers.Role.objects.getAvailableRolesByManufacture(manufacture = self.manufacture)
